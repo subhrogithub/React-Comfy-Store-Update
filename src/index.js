214d6e3e347d1a8389f55a99a27e995c7af58c4d
subhro <subhro1smita@gmail.com>
2021-08-28 20:01:05 +0530

    Auth0 added

--- src/index.js
+++ src/index.js
@@ -8,13 +8,13 @@ import { CartProvider } from './context/cart_context'
 import { UserProvider } from './context/user_context'
 import { Auth0Provider } from '@auth0/auth0-react'
 import { SpeechProvider } from '@speechly/react-client'
-// dev-pu8wyk-g.us.auth0.com
-// 7vHgXJ01aWGyMVLWdZUwQSUapigdjfKu
+// dev-erosrb8n.us.auth0.com
+// o0eZsjpA4o3qihhvQo2IoVY2HYZVxe2M
 ReactDOM.render(
   <SpeechProvider appId="815a8a5c-1ef3-4d00-916d-1948c9dd0a0a" language="en-US">
   <Auth0Provider
-    domain={process.env.REACT_APP_AUTH_DOMAIN}
-    clientId={process.env.REACT_APP_AUTH_CLIENT_ID}
+    domain='o0eZsjpA4o3qihhvQo2IoVY2HYZVxe2M'
+    clientId='o0eZsjpA4o3qihhvQo2IoVY2HYZVxe2M'
     redirectUri={window.location.origin}
     cacheLocation='localstorage'
   >
